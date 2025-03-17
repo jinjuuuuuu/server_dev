6fab1acffe3a64332993fae5cd3c4fd1088151c5
한진주 <sujinhan@Susies-MacBook.local>
2025-03-17 13:25:26 +0900

    add celery.py

--- server_dev/settings.py
+++ server_dev/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = "redis://localhost:6379/0"  # Redis를 브로커로 사용
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
