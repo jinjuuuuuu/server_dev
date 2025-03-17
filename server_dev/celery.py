e2c4cbef9a73e3be77a64108e1f3584e750b6b11
Jinju <143995943+jinjuuuuuu@users.noreply.github.com>
2025-03-17 04:41:39 +0000

    working on connecting celery

--- server_dev/celery.py
+++ server_dev/celery.py
@@ -1,4 +1,10 @@
 import os
 from celery import Celery
 
-os.environ.setdefault()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server_dev.settings")
+
+celery_app = Celery("server_dev")
+
+celery_app.config_from_object("django.conf:settings", namespace="CELERY")
+
+celery_app.autodiscover_tasks()
